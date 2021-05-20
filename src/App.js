57e335210508adee6273338376c70b0671be81b6
Landon Whitesides <whitesideslandon@gmail.com>
2021-05-20 14:53:44 -0600

    added styling and sign up button

--- src/App.js
+++ src/App.js
@@ -33,53 +33,60 @@ function App() {
   }
 
   return (
-    <div>
+    <>
+        <a href="#sign-up">
+          <div class="sign-up-button">
+            <p>Sign Up Now!</p>
+          </div>
+        </a>
+    <div class="container">
+
+
+
       <div>
-        <h1>1st Annual Whitesides Memorial Day 10K.</h1>
+        <h1>1st Annual Whitesides Memorial Day 10K</h1>
         <h2>May 31st, 2021</h2>
-        <h2>Start Time: 11:00</h2>
+        <h2>Start Time: 11:00 AM</h2>
       </div>
+
+      <hr></hr>
 
       <div>
         <h1>Race Description</h1>
 
-        <p>I usually do this run every year alone on Memorial day but I've decided to open it up to the world this year. 
-          Show up at my house at 10:30ish. I'll be giving out these little flags this year to run with since it's Memorial day.
-          The race will start at 11:00 am. We'll run straight down 100 West to 400 North, then 
-          turn right for a mile, then up Meridian for the last 3/4 mile or so. Its not a true 10k, its more like 5.7 miles, but
-          if you run far enough back in the cemetary you can make it to 6 miles. Note that this is not a loop so you'll need to arrange
-          for someone to pick you up at the Cemetary whenever you finish. I usually just meet my family out there and we visit 
-          my grandparents graves together. 
-        </p>
+        <p>I usually do this run every year alone on Memorial day but I've decided to open it up to the world this year.</p> 
+
+          <p>Show up at my house at 10:30ish. I'll be giving out these little flags this year to run with since it's Memorial day.
+          The race will start at 11:00 am.</p>
+
+          <img src="https://mcusercontent.com/b61bde706ca219f6690032ed4/images/763a148e-e238-1620-d439-164f86baad4c.jpg" alt="picture of little flag"></img>
+          
+          <p>We'll run straight down 100 West to 400 North, then turn right for a mile, then up Meridian for the last 3/4 mile or so.
+            <br/>
+            <a href="https://www.google.com/maps/d/u/0/edit?mid=1OIl8cYbFJz1bpZUM_HXsQN56am-sVhJw&usp=sharing" target="_blank">Click Here for Directions.</a>
+          </p>
+         
+          <p>Its not a true 10k, its more like 5.7 miles, but if you run far enough back in the cemetary you can make it to 6 miles.</p> 
+          
+          <p>Note that this is not a loop so you'll need to arrange for someone to pick you up at the Cemetary whenever you finish. 
+          I usually just meet my family out there and we visit my grandparents graves together. </p>
+        
 
         <h1>Non-Runners/Walkers</h1>
-        <p>If you don't want to run, that's completely ok. This race is open to any and all modes of transportation (except cars).
+        <p id="sign-up">If you don't want to run, that's completely ok. This race is open to any and all modes of transportation (except cars).
           This includes bicycles, scooters, roller blades, motorcycles, golf carts, side x sides, 4-wheelers or anything else you 
           can think of that's not a car... 
         </p>
+      </div>  
 
-        <h2>Start Address</h2>
-        <p>My House</p>
-        <p>216 Countryside Dr.</p>
-        <p>Rupert, ID 83350</p>
-
-        <h2>End Address</h2>
-        <p>The Rupert Cemetary</p>
-        <p>450 N. Meridian</p>
-        <p>Rupert, ID 83350</p>
-
-        <h2>Route</h2>
-        <div>Map goes here</div>
-
-      </div>
-
-      <div>
+      <div class="sign-up-div">
         <h1>Sign up!</h1>
-        <form onSubmit={handleSubmit}>
-          <label>Name</label>
-          <input onChange={(e)=>{setName(e.target.value)}}value={name}/>
-          <label>Select your Mode of transportation:</label>
-          <select 
+        <form class="sign-up-form" onSubmit={handleSubmit}>
+          <label class="form-element">Name</label>
+          <input class="form-element" onChange={(e)=>{setName(e.target.value)}}value={name}/>
+          <label class="form-element custom-select">Select your Mode of transportation:</label>
+          <select
+              class = "form-element" 
               onChange={(e)=>{
               setMode(e.target.value)
               console.log(e.target.value)}} 
@@ -91,11 +98,29 @@ function App() {
               )
             })}
           </select>
-          <button type="submit">Submit</button>
+          <button class="form-element" type="submit">Submit</button>
         </form>
       </div>
+
+      <hr/>
+
+      <div>     
+        <h2>Start Address</h2>
+        <p><b>My House</b></p>
+        <p>216 Countryside Dr.</p>
+        <p>Rupert, ID 83350</p>
+
+        <h2>End Address</h2>
+        <p><b>The Rupert Cemetary</b></p>
+        <p>450 N. Meridian</p>
+        <p>Rupert, ID 83350</p>
+
+        <h2>Route</h2>
+          <iframe src="https://www.google.com/maps/d/embed?mid=1OIl8cYbFJz1bpZUM_HXsQN56am-sVhJw" width="100%" height="480"></iframe>
+      </div> 
      
     </div>
+    </>
   );
 }
 
